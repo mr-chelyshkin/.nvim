fb8b05283f7a518b37b5b5e7f7449665ad6b3b5e
mr-chelyshkin <mr.chelyshkin@icloud.com>
2023-08-29 23:23:20 +0300

    add nvim-treesitter as plugin

--- plugins.lua
+++ plugins.lua
@@ -21,6 +21,18 @@ local plugins = {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = {"go", "python"},
+        highlight = {
+          enable = true,
+        },
+      }
+    end,
+  },
 
   -- Python plugins
   {
